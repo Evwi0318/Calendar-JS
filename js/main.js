8a06fcc6356ca16b3c35030b36718c87861cc2a3
Isak W <isakwarsame3@gmail.com>
2026-05-05 20:59:18 +0200

    feat: add todo creation with form and list rendering

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { renderCalendar } from './calendar.js';
 import { renderTodayDate } from './today.js';
+import { initTodos } from './todo.js';
 
 renderTodayDate();
 renderCalendar();
+initTodos();
